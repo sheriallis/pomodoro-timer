23f03bed82fd6500dfb37680d43114dbf717c467
Sheri Richardson <shrdson@gmail.com>
2019-06-19 22:54:48 +0200

    Implement UI design

--- src/index.js
+++ src/index.js
@@ -4,8 +4,53 @@ import './styles.css';
 
 function App() {
   return (
-    <div className="App">
+    <div id="App">
       <h1>Pomodoro Timer</h1>
+      <div id="timer-container">
+        <header className="flex-center">
+          <h2 id="timer-label">Session</h2>
+          <div id="time-left">25:00</div>
+          <div className="wrapper">
+            <button id="start_stop">
+              <i className="fas fa-play" /> Start
+            </button>
+            <button id="reset">
+              <i className="fas fa-sync-alt" /> Reset
+            </button>
+          </div>
+        </header>
+        <footer>
+          <div className="time-adjust-wrapper">
+            <h3 id="break-label">Session Length</h3>
+            <div class="adjust-time">
+              <span id="session-increment">
+                <i className="fas fa-arrow-circle-up" />
+              </span>
+              <p>
+                <span id="session-length"> 25</span> min
+              </p>
+              <span id="session-decrement">
+                <i className="fas fa-arrow-circle-down" />
+              </span>
+            </div>
+          </div>
+
+          <div className="time-adjust-wrapper">
+            <h3 id="session-label">Break Length</h3>
+            <div className="adjust-time">
+              <span id="break-increment">
+                <i className="fas fa-arrow-circle-up" />
+              </span>
+              <p>
+                <span id="break-length"> 5</span> min
+              </p>
+              <span id="break-decrement">
+                <i className="fas fa-arrow-circle-down" />
+              </span>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
