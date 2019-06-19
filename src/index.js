d657283f16cd401a0e230a9a97c0f65d431f68fc
Sheri Richardson <shrdson@gmail.com>
2019-06-19 19:22:22 +0200

    Basic React setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pomodoro Timer</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
